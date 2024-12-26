9ae30163594414c5329de479ccb369ab7dce7a98
MoaazEmam <moaaz.emam06@eng-st.cu.edu.eg>
2024-12-26 16:56:15 +0200

    Revert "This is my designer code. trying to push"
    This reverts commit 714bd675e531fdf1e5f74bec1e7470fc2fe4e540.

--- Soleil et Soie/Soleil et Soie/LogIn.cs
+++ Soleil et Soie/Soleil et Soie/LogIn.cs
@@ -20,26 +20,13 @@ namespace Soleil_et_Soie
             controllerObj = new Controller();
         }
 
-
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            string type = "";
-            bool exists = controllerObj.GetLogin(textBoxUserName.Text, textBoxPass.Text, ref type);
-            if (!exists)
-            {
+            bool exists = controllerObj.GetLogin(textBoxUserName.Text, textBoxPass.Text);
+            if (!exists) {
                 labelError.Visible = true;
             }
-            else if (type == "designer")
-            {
-                int desID = controllerObj.GetUserID(textBoxUserName.Text, textBoxPass.Text);
-                designerhome designerhome = new designerhome( textBoxUserName.Text, desID);
-                designerhome.Show();
-               // designer des = new designer();
-                //des.Show();
-                
-            }
         }
-
 
         private void linkLabelsignin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
